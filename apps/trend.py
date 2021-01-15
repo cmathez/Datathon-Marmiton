f5ddbf0faeaa8f840b0992dbef187af310972f96
cmathez <61285819+cmathez@users.noreply.github.com>
2021-01-15 10:16:53 +0100

    Update trend.py

--- apps/trend.py
+++ apps/trend.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from index import app
+from app import app
 
     # To maniplutate data
 
