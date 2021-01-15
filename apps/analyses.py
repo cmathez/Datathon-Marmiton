6557a526bac5595bff876f844c17c95a0b4bdbb4
cmathez <61285819+cmathez@users.noreply.github.com>
2021-01-15 03:47:45 +0100

    Update analyses.py

--- apps/analyses.py
+++ apps/analyses.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from app import app
+from index import app
 
     # To maniplutate data
 
