d5c162791a435b16cb0d255ff54a4414250f2308
cmathez <61285819+cmathez@users.noreply.github.com>
2021-01-15 03:49:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from apps import analyses, find_recipe, trend
-from dash import app
+from index import app
 
 ### Create Dash architecture ###
 
